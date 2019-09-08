ac72363133df5aa959cc2103e0226a2092482255
amir <dev.amirhemmati@yahoo.com>
2019-09-08 17:40:45 +0430

    add

--- asset/js/sendmassage.js
+++ asset/js/sendmassage.js
@@ -4,15 +4,17 @@ forms.addEventListener("submit",(e) => {
     e.preventDefault();
     const name = forms["name"];
     const email = forms["email"];
-    const massage = document.querySelector("#txtarea");
-    if(name.value == "" && email.value == "" && massage.value == ""){
+    const massages = document.querySelector("#txtarea");
+
+
+    if(name.value == "" && email.value == "" && massages.value == ""){
         name.style.borderBottom = '1px solid red';
         email.style.borderBottom = '1px solid red';
-        massage.style.borderBottom = '1px solid red';
+        massages.style.borderBottom = '1px solid red';
         setTimeout(() => {
             name.style.borderBottom = 'solid 1px #dfdfdf';
             email.style.borderBottom = 'solid 1px #dfdfdf';
-            massage.style.borderBottom = 'solid 1px #dfdfdf';
+            massages.style.borderBottom = 'solid 1px #dfdfdf';
         }, 4000);
     }
     else if(name.value == "" && email.value == ""){
@@ -23,20 +25,20 @@ forms.addEventListener("submit",(e) => {
             email.style.borderBottom = 'solid 1px #dfdfdf';
         }, 4000);
     }
-    else if(name.value == "" && massage.value == ""){
+    else if(name.value == "" && massages.value == ""){
         name.style.borderBottom = '1px solid red';
-        massage.style.borderBottom = '1px solid red';
+        massages.style.borderBottom = '1px solid red';
         setTimeout(() => {
             name.style.borderBottom = 'solid 1px #dfdfdf';
-            massage.style.borderBottom = 'solid 1px #dfdfdf';
+            massages.style.borderBottom = 'solid 1px #dfdfdf';
         }, 4000);
     }
-    else if(email.value == "" && massage.value == ""){
+    else if(email.value == "" && massages.value == ""){
         email.style.borderBottom = '1px solid red';
-        massage.style.borderBottom = '1px solid red';
+        massages.style.borderBottom = '1px solid red';
         setTimeout(() => {
             email.style.borderBottom = 'solid 1px #dfdfdf';
-            massage.style.borderBottom = 'solid 1px #dfdfdf';
+            massages.style.borderBottom = 'solid 1px #dfdfdf';
         }, 4000);
     }else if(name.value == ""){
         name.style.borderBottom = '1px solid red';
@@ -50,10 +52,10 @@ forms.addEventListener("submit",(e) => {
             email.style.borderBottom = 'solid 1px #dfdfdf';
         }, 4000);
     }
-    else if(massage.value == ""){
-        massage.style.borderBottom = '1px solid red';
+    else if(massages.value == ""){
+        massages.style.borderBottom = '1px solid red';
         setTimeout(() => {
-            massage.style.borderBottom = 'solid 1px #dfdfdf';
+            massages.style.borderBottom = 'solid 1px #dfdfdf';
         }, 4000);
     }
     else{
@@ -62,13 +64,13 @@ forms.addEventListener("submit",(e) => {
             To : "berlinproject2018@gmail.com",
             From : "berlinproject2018@gmail.com",
             Subject : `your name: ${name.value}`,
-            Body : `Hello your send massage:<hr>${massage.value} <br> ${email.vlaue}`
+            Body : `Hello your send massage:<hr>${massages.value} <br> ${email.vlaue}`
         }).then(
         message => {
             alert('your email is: ' + massage)
             name.value = "";
             email.value = "";
-            massage.value = "";
+            massages.value = "";
         }
         );
     }
